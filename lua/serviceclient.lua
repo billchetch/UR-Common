8db0d9500f172badaa5780e3a5ec5f4becdfc453
billchetch <45006134+billchetch@users.noreply.github.com>
2020-08-09 21:30:23 +0800

    s

--- lua/serviceclient.lua
+++ lua/serviceclient.lua
@@ -33,7 +33,6 @@ function ServiceClient(serviceName)
 	function self.connect(i , p)
 		self.base.connect(i, p);
 		self.serviceReady = false;
-
 		if self.isConnected() then
 			self.requestServiceStatus();
 		end
